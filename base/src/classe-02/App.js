0085ffc3e99cd2e48dd8afce30efd47ab7d49e0e
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-09-09 19:25:18 -0300

    Card de usuário

--- base/src/classe-02/App.js
+++ base/src/classe-02/App.js
@@ -1,6 +1,52 @@
+import { useState } from 'react';
+
+import kelvinCosta from './assets/kelvin-costa.png';
+
+function Card({ foto, nome, insta, seguidores, seguindo }) {
+  return (
+    <div className="card">
+      <img className="card__foto" src={foto} alt="Foto de perfil" />
+      <h1 className="card__nome">{nome}</h1>
+      <span className="card__insta">{insta}</span>
+      <span className="card__seguidores">{seguidores} seguidores</span>
+      <span className="card__seguindo">{seguindo} seguindo</span>
+    </div>
+  );
+}
+
 function App() {
+  const [seguidores, setSeguidores] = useState(140);
+  const [btnSeguir, setBtnSeguir] = useState('btn-seguir--seguir');
+  const [textoBtn, setTextoBtn] = useState('Seguir');
+
+  function seguir() {
+    const estaSeguindo = btnSeguir === 'btn-seguir--seguir' ? 'btn-seguir--seguindo' : 'btn-seguir--seguir';
+    const texto = textoBtn === 'Seguir' ? 'Seguindo' : 'Seguir';
+    const numSeguidores = texto === 'Seguindo' ? seguidores + 1 : seguidores - 1;
+
+    setBtnSeguir(estaSeguindo);
+    setTextoBtn(texto);
+    setSeguidores(numSeguidores);
+  }
+
+  const usuario = {
+    foto: kelvinCosta,
+    nome: 'Kelvin Costa',
+    insta: '@costa',
+    seguidores: seguidores,
+    seguindo: 207
+  };
+
   return (
     <div className="App">
+      <Card
+        foto={usuario.foto}
+        nome={usuario.nome}
+        insta={usuario.insta}
+        seguidores={usuario.seguidores}
+        seguindo={usuario.seguindo}
+      />
+      <button className={`btn-seguir ${btnSeguir}`} onClick={seguir}>{textoBtn}</button>
     </div>
   );
 }
