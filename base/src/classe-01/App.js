5a4edc1e8b6e0972ac84a109b1e30555d1f9bfd4
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-09-09 18:29:46 -0300

    Alerta de cookie

--- base/src/classe-01/App.js
+++ base/src/classe-01/App.js
@@ -1,6 +1,31 @@
+import { useState } from 'react';
+
+import close from './assets/close.svg';
+import cookie from './assets/cookie.svg';
+
+function Card(props) {
+  const [hidden, setHidden] = useState('');
+
+  function fechar() {
+    setHidden('hidden');
+  }
+
+  return (
+    <div className={`card ${hidden}`}>
+      <img className="card__close" src={close} alt="fechar" onClick={fechar} />
+      <img className="card__img" src={cookie} alt="cookie" />
+      <p className="card__text">{props.texto}</p>
+      <button className="card__btn" onClick={fechar}>{props.children}</button>
+    </div>
+  );
+}
+
 function App() {
+  const cardTexto = 'Nós utilizamos cookies para melhorar nossa UX, analytics e marketing.';
+
   return (
     <div className="App">
+      <Card texto={cardTexto}>Tudo Bem!</Card>
     </div>
   );
 }
